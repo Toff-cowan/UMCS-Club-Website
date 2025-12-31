5ef6363ca9bc0d03bab6cb1a7faeb4458e40cddf
Nique! <anniquepatterson912@gmail.com>
2025-12-30 19:02:11 -0500

    Create Event model for club events management
    This model is essential for managing and retrieving event data for the UMCS Club Website.

---
+++ backend/models/event.js
@@ -0,0 +1,30 @@
+// backend/models/event.js
+//  This backend model represents the events in the database that we utilize to display on the section of the frontend pertaining to the
+// various club events.
+// The schema contains the title, image, description and the date they were created in the database.
+// This model is essential for managing and retrieving event data for the UMCS Club Website.
+// It allows for structured storage and access to event information.
+// This setup ensures that event data is consistently formatted and easily retrievable for display on the frontend.
+// It is built using Mongoose, a popular ODM for MongoDB in Node.js applications.
+
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String // URL or file path
+  },
+  description: {
+    type: String,
+    maxlength: 500
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model("Event", EventSchema);
