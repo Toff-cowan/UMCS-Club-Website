25916a4a3676ffa15649a8afc850a681c8dd63f6
JGreen00 <greenjoshua385@gmail.com>
2026-01-06 21:25:32 -0500

    About Page starter code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import About from '.frontend/src/About' //directory location for About 
 
 const App = () => {
   return (
     <div>
+      <About />
       
     </div>
   )
 }
 
-export default App
+export default App;
