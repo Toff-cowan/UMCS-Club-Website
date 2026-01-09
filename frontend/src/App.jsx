893b05036c0ce22d338dabcc1c5dd00e7c49f9e5
Toff-cowan <cowanchristoff24@gmail.com>
2026-01-08 20:02:11 -0500

    Fontend Setup Modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from '.frontend/src/About' //directory location for About 
+import Layout from './components/Layout' //  Layout Directory
+import Navbar from './components/Frame/Navbar' //directoory for the Navbar 
+import Footer from './component/Frame/Footer' // directory for the Footer
 
 const App = () => {
   return (
-    <div>
-      <About />
-      
+    <div className='App'> 
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/about" element={<About />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ frontend/src/components/Frame/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as assets from '../../assets';
+
+const navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default navbar
