525b4e6a1f4e64015233d83707052cf2e90d8baf
Toff-cowan <cowanchristoff24@gmail.com>
2026-01-20 21:47:37 -0500

    navbar testing

--- frontend/src/components/Frame/Navbar.jsx
+++ frontend/src/components/Frame/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import logo from '../../assets/UMCS Logo.png';
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/sigs" className="nav-link">SIGs</Link>
-            <Link to="/about" className="nav-link">About</Link>
-            <Link to="/projects" className="nav-link">Projects</Link>
-            <Link to="/executives" className="nav-link">Executives</Link>
-        </nav>
-    );
+    return null;
+    // return (
+    //     <nav className="navbar">
+    //        {} <Link to="/" className="navbar-logo">
+    //             <img src={logo} alt="UMCS Logo" />
+    //         </Link>
+    //         <div className="navbar-links">
+    //             <Link to="/" className="nav-link">Home</Link>
+    //             <Link to="/sigs" className="nav-link">SIGs</Link>
+    //             <Link to="/about" className="nav-link">About</Link>
+    //             <Link to="/projects" className="nav-link">Projects</Link>
+    //             <Link to="/executives" className="nav-link">Executives</Link>
+    //         </div>
+    //     </nav>
+    // );
 };
 export default Navbar;
