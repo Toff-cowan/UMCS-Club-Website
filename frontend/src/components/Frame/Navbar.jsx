a63a047c9cfb0f85de3b47ee3599f9664b889f72
shannaeharris06 <shannaeharris06@gmail.com>
2026-01-20 21:10:13 -0500

    Navbar modifications

---
+++ frontend/src/components/Frame/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/sigs" className="nav-link">SIGs</Link>
+            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/projects" className="nav-link">Projects</Link>
+            <Link to="/executives" className="nav-link">Executives</Link>
+        </nav>
+    );
+};
+export default Navbar;
