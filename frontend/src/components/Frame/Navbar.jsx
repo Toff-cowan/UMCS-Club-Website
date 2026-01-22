de6743de031bb7c18b34de72e3617a5d05344881
shannaeharris06 <shannaeharris06@gmail.com>
2026-01-21 19:56:22 -0500

    navbar changes 2

--- frontend/src/components/Frame/Navbar.jsx
+++ frontend/src/components/Frame/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import logo from '../../assets/UMCS Logo.png';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
+            <Link to="/" className="navbar-logo">
+                <img src={logo} alt="UMCS Logo" />
+            </Link>
             <Link to="/" className="nav-link">Home</Link>
             <Link to="/sigs" className="nav-link">SIGs</Link>
             <Link to="/about" className="nav-link">About</Link>
             <Link to="/projects" className="nav-link">Projects</Link>
-            <Link to="/executives" className="nav-link">Executives</Link>
+            <Link to="/executives" className="nav-link">Executives</Link>        
         </nav>
     );
 };
